22d0b21bb8f871e31bfcb17cb34b1b0a20ae94b9
Narra Prathap <narra.prathap@acqueon.com>
2025-03-10 00:21:31 +0530

    Set up GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
+//vitejs.dev/config/
 export default defineConfig({
+  base: "TradingJournal_Application",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
